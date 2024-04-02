6b79e5c925dc89a306e3f5c8cd2e22f814e024f3
Medet Ahmetson <milayter@gmail.com>
2024-04-03 01:00:02 +0700

    Add mistral

---
+++ mistral.py
@@ -0,0 +1,47 @@
+# interpreter.py
+from dotenv import load_dotenv
+load_dotenv("./env")
+
+from fastapi import FastAPI
+import message
+import os
+from datetime import datetime
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+from pyjsparser import parse
+
+api_key = os.environ["MISTRAL_API_KEY"]
+print("Mistral API KEY: ", api_key)
+model = "mistral-tiny"
+
+client = MistralClient(api_key=api_key)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return message.correct_answer("")
+
+@app.get("/implement")
+def add(code_gen_task: str):
+    return {
+        "code": "document.body.backgroundColor = 'blue'",
+        "correct": True
+    }
+    code_prompt = message.code_prompt(code_gen_task)
+
+    code_response = client.chat(
+        model=model,
+        messages=[ChatMessage(role="user", content=code_prompt)],
+    )
+    output = message.clean_code(code_response.choices[0].message.content)
+
+    result = message.correct_answer(output)
+
+    try:
+        parse(output)
+        result.correct = True
+    except:
+        result.correct = False
+
+    return result
